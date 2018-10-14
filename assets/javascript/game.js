a45f2837298792cefbbcf3dc1f3b29d7ae90d0f9
Paige Herbst <PMoney@Paiges-MacBook-Pro.local>
2018-10-13 20:34:57 -0600

    edited

---
+++ assets/javascript/game.js
@@ -0,0 +1,207 @@
+$(document).ready(function() {
+    var i = [];
+    for (var i = 19; i<121; i++) {
+        random.push(i);
+    }
+
+    var crystals = [];
+    for (var c = 1; c < 13; c++) {
+        crystals.push(c);
+    }
+
+    var randomNumber;
+    var crystalNumbers = [];
+    var c1;
+    var c2;
+    var c3;
+    var c4;
+
+var yourTotalScore = 0;
+    var wins = 0;
+    var losses = 0;
+
+    function pickRandomNum(arr) {
+        var x = arr[Math.floor(Math.random() * arr.length)];
+        randomNumber = x;
+        $("#randomNumber").html(randomNumber);
+    }
+    function pickRandomCrystals(arr) {
+
+		for (var y = 0; y < 4; y++){
+
+			var a = arr[Math.floor(Math.random() * arr.length)];
+
+			crystalNumbers.push(a);
+        }
+    function crystalValues(arr) {
+        for (i = 0; i < arr.length; i++) {
+             $("#button-" + (i+1)).attr("value", arr[i]);
+            }
+            c1 = arr[0];
+            c2 = arr[1];
+            c3 = arr[2];
+            c4 = arr[3];
+        }    
+
+    function gameReset(x) {
+
+        crystalNumbers = []; // clears crystal number values
+    
+        pickRandomNumber(rand);
+    
+        pickRandomCrystals(crystals);
+    
+        crystalValues(crystalNumbers);
+    
+        totalScore = 0;
+        $("#totalNumber").html(totalScore);
+        alert(x);
+        } 
+    pickRandomNumber(rand); // random number to match
+    pickRandomCrystals(crystals); // array of random crystal values
+    crystalValues(crystalNumbers);
+
+    $("btn btn-danger").on("click", function() {
+
+        totalScore += c1;
+        $("#totalNumber").html(totalScore);
+    });
+
+    $("btn btn-warning").on("click", function() {
+
+        totalScore += c2;
+        $("#totalNumber").html(totalScore);
+    });
+
+    $("btn btn-success").on("click", function() {
+
+        totalScore += c3;
+        $("#totalNumber").html(totalScore);
+    });
+
+    $("btn btn-primary").on("click", function() {
+
+        totalScore += c4;
+        $("#totalNumber").html(totalScore);
+    });
+
+$("button").on("click", function() {
+    // this is what happens if the user wins
+    if (totalScore == randNumber) {
+
+        wins++;
+        console.log(totalScore);
+        $("#totalNumber").html(totalScore);
+        $("#wins").html("Wins: " + wins);
+
+
+        setTimeout(function() {gameReset("YOU WIN!!")}, 200);
+    }
+
+    else if (totalScore > randNumber){
+
+        losses++;
+        $("#totalNumber").html(totalScore);
+        $("#losses").html("Losses: " + losses);
+// var num1= $(".red");
+// var num2= $(".yellow");
+// var num3= $(".green");
+// var num4= $(".blue");
+
+
+// $( document ).ready(function(){
+//     var Random=Math.floor(Math.random()*102+19)
+    
+//     // Display random number
+//     $("#yourTargetScore").text(Random);
+    
+//     //Generate random number for each crystal
+//     var num1= Math.floor(Math.random()*12+1)
+//     var num2= Math.floor(Math.random()*12+1)
+//     var num3= Math.floor(Math.random()*12+1)
+//     var num4= Math.floor(Math.random()*12+1)
+    
+//     // Variables to keep track of wins, losses and total
+//     var yourTotalScore= 0; 
+//     var wins= 0;
+//     var losses = 0;
+    
+  
+//   $("#wins").text(wins);
+//   $("#losses").text(losses);
+  
+//   // Reset game
+//   function reset(){
+//         Random=Math.floor(Math.random()*102+19);
+//         console.log(Random)
+//         $("#yourTargetScore").text(Random);
+//         num1= Math.floor(Math.random()*12+1);
+//         num2= Math.floor(Math.random()*12+1);
+//         num3= Math.floor(Math.random()*12+1);
+//         num4= Math.floor(Math.random()*12+1);
+//         playerTotal= 0;
+//         $("#yourTotalScore").text(playerTotal);
+//         } 
+  // Display wins
+//   function woohoo(){
+//   alert("Congrats! You won!");
+//     wins++; 
+//     $('#wins').text(wins);
+//     reset();
+//   }
+//   // Display losses
+//   function loser(){
+//   alert ("Sorry! You lose!");
+//     losses++;
+//     $('#losses').text(losses);
+//     reset()
+//   }
+  // Clicking crystals
+    // $("button-red").on ("click", function(){
+    //   playerTotal = playerTotal + num1;
+      
+    //   $("#yourTotalScore").text(playerTotal); 
+            //Win & lose conditions
+    //       if (playerTotal == Random){
+    //         woohoo();
+    //       }
+    //       else if ( playerTotal > Random){
+    //         loser();
+    //       }   
+    // })  
+    // $(".blue").on ('click', function(){
+    //   playerTotal = playerTotal + num2;
+    //   console.log("#yourTotalScore= " + playerTotal);
+    //   $("#yourTotalScore").text(playerTotal); 
+        //   if (playerTotal == Random){
+        //     woohoo();
+        //   }
+        //   else if ( playerTotal > Random){
+        //     loser();
+        //   } 
+    // })  
+    // $(".yellow").on ('click', function(){
+    //   playerTotal = playerTotal + num3;
+    //   console.log("#yourTotalScore= " + playerTotal);
+    //   $("#yourTotalScore").text(playerTotal);
+  
+        //     if (playerTotal == Random){
+        //     woohoo();
+        //   }
+        //   else if ( playerTotal > Random){
+        //     loser();
+        //   } 
+    // })  
+    // $(".green").on ('click', function(){
+    //   playerTotal = playerTotal + num4;
+    //   console.log("#yourTotalScore= " + playerTotal);
+    //   $("#yourTotalScore").text(playerTotal); 
+        
+        //     if (playerTotal == Random){
+        //     woohoo();
+        //   }
+        //   else if ( playerTotal > Random){
+        //     loser();
+        //   }
+    // });   
+}); 
